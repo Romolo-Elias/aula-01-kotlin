4791336b2416b076e002a36e3c984b93322a4ac9
Romolo Elias <romolopsn2004@gmail.com>
2026-08-24 02:15:37 -0300

    Home screen

---
+++ app/src/main/java/br/com/fiap/recipes/screens/HomeScreen.kt
@@ -0,0 +1,255 @@
+package br.com.fiap.recipes.screens
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.com.fiap.recipes.ui.theme.RecipesTheme
+import br.com.fiap.recipes.R
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+    Surface (modifier = Modifier
+                        .fillMaxSize()
+    ){
+        Scaffold(
+            topBar = {MyTopAppBar()},
+            bottomBar = {MyBottomAppBar()},
+            floatingActionButton = {
+                FloatingActionButton(
+                    onClick = {},
+                    shape = CircleShape,
+                    containerColor = MaterialTheme.colorScheme.primary
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = "Add button"
+                    )
+                }
+            }
+
+        ) { paddingValues ->
+            ContentScreen(modifier = Modifier.padding(paddingValues))
+            }
+        }
+    }
+
+@Preview
+@Composable
+private fun HomeScreenPreview() {
+    RecipesTheme {
+        HomeScreen()
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyTopAppBar(modifier: Modifier = Modifier) {
+    TopAppBar(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(80.dp),
+        title = {
+            Row(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Column {
+                    Text(
+                        text = "Hello, João!",
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = MaterialTheme.colorScheme.primary,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(
+                        text = "joão@email.com",
+                        style = MaterialTheme.typography.displaySmall
+                    )
+                }
+                Card (
+                    modifier = Modifier.size(48.dp),
+                    shape = CircleShape,
+                    colors = CardDefaults
+                        .cardColors(
+                            containerColor = Color.Transparent
+                        ),
+                    border = BorderStroke(
+                        width = 1.dp,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                ){
+                    Image(
+                        painter = painterResource(R.drawable.user),
+                        contentDescription = "User Image"
+                    )
+                }
+            }
+        }
+    )
+}
+
+
+@Preview
+@Composable
+private fun MyTopAppBarPreview() {
+    RecipesTheme {
+        MyTopAppBar()
+    }
+}
+
+data class BottomNaviationItem(
+    val tittle: String,
+    val icon: ImageVector
+)
+
+@Composable
+fun MyBottomAppBar(modifier: Modifier = Modifier) {
+    val items = listOf(
+        BottomNaviationItem("Home", icon = Icons.Default.Home),
+        BottomNaviationItem("Favorites", icon = Icons.Default.Favorite),
+        BottomNaviationItem("Profile", icon = Icons.Default.Person),
+    )
+
+    NavigationBar(
+        containerColor = MaterialTheme.colorScheme.tertiary
+    ) {
+        items.forEach {item->
+            NavigationBarItem(
+                selected = false,
+                onClick = {},
+                icon = {
+                    Icon(
+                        imageVector = item.icon,
+                        contentDescription = item.tittle,
+                        tint = MaterialTheme.colorScheme.onTertiary,
+                        modifier = Modifier.size(24.dp)
+                    )
+                },
+                label = {
+                    Text(
+                        text = item.tittle,
+                        style = MaterialTheme.typography.displaySmall,
+                        color = MaterialTheme.colorScheme.onTertiary
+                        )
+                }
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun MyBottomAppBarPreview() {
+    RecipesTheme {
+        MyBottomAppBar()
+    }
+}
+
+@Composable
+fun ContentScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp)
+    ) {
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 8.dp),
+            label = {
+                Text(text = stringResource(R.string.search_by_recipes),
+                    style = MaterialTheme.typography.labelSmall)
+            },
+            shape = RoundedCornerShape(12.dp),
+            colors = OutlinedTextFieldDefaults
+                .colors(
+                    unfocusedBorderColor = Color.Transparent,
+                    unfocusedContainerColor = Color(0xfff5f5f5)
+                    ),
+            trailingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = stringResource(R.string.search_icon),
+                )
+            }
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+        Card(modifier = Modifier
+            .padding(bottom = 16.dp)
+            .fillMaxWidth()
+            .height(116.dp)) {
+            Image(
+                painter = painterResource(R.drawable.cooking_card),
+                contentDescription = "cooking",
+                modifier = Modifier
+                    .fillMaxWidth(),
+                contentScale = ContentScale.Crop
+            )
+        }
+        Text(
+            text = stringResource(R.string.categories),
+            style = MaterialTheme.typography.labelMedium,
+            color = MaterialTheme.colorScheme.primary
+        )
+        Spacer(modifier = Modifier.height(116.dp))
+        Text(
+            text = stringResource(R.string.newly_added_recipes),
+            style = MaterialTheme.typography.labelMedium,
+            color = MaterialTheme.colorScheme.primary
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ContentScreenPreview() {
+    RecipesTheme {
+        ContentScreen()
+    }
+}
